eae3868d0c467b9c1d58082481b56d99eddf8bcb
Nontapat <Antauribp@gmail.com>
2024-04-11 12:50:48 +0700

    Make max 5 tickets per person

--- validate.js
+++ validate.js
@@ -9,7 +9,7 @@ function checkNID() {
 
 function checkTicketNo() {
   let num = (document.getElementById("ticknum").value).trim();
-  if (isNaN(num)) {
+  if (isNaN(num) || num <=0 || num >=6) {
     return false;
   } else {
 	return true;
