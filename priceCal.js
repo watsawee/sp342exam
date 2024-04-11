89f9fad011702d4a4f5d44c0d448adafb6311a8c
Thanat <tk.2546thanat.1@gmail.com>
2024-04-11 10:51:03 +0700

    Fix funtion priceCalculate()

--- priceCal.js
+++ priceCal.js
@@ -2,8 +2,28 @@ function priceCalculate() {
   let ticknum = (document.getElementById("ticknum").value).trim();
   let selectObj = document.getElementById("event");
   let eventId = parseInt(selectObj.options[selectObj.selectedIndex].value);
+  let eventPrice = 0;
   
-  return 0;
+  switch(eventId) {
+    case 1:
+      eventPrice = 91.25;
+      break;
+    case 2:
+      eventPrice = 79.88;
+      break;
+    case 3:
+      eventPrice = 83.12;
+      break;
+    case 4:
+      eventPrice = 100;
+      break;
+    default:
+      eventPrice = 0;
+      break;
+  }
+  
+  let totalPrice = eventPrice * ticknum;
+  return totalPrice.toFixed(2);
 }
 
 
