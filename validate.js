cfc434e81e2183fda9af5e5140f3a84cbcb228d6
rinadee <Rinradee3010@gmail.com>
2024-04-11 10:52:59 +0700

    3.Edit ticket max5

--- validate.js
+++ validate.js
@@ -1,7 +1,7 @@
 
 function checkNID() {
-  let nid = (document.getElementById("nid").value = "").trim();
-  if (nid.length != 13)  {
+  let nid = (document.getElementById("nid").value ).trim();
+  if (nid.length != 13 && nid >= 1 && nid <=13 )  {
     return false;
   } else { 
 	return true;
@@ -30,6 +30,7 @@ function validateForm(){
 		}else{
 			total = priceCalculate();
 			alert("Total price for this booking is "+total+" USD");
+			
 			return false;
 		}
 	}
