791a1064be53edc882662c6713565b9b4b529c2b
bbento2 <benyawan014@gmail.com>
2024-04-11 12:40:04 +0700

    add page2

--- priceCal.js
+++ priceCal.js
@@ -3,7 +3,20 @@ function priceCalculate() {
   let selectObj = document.getElementById("event");
   let eventId = parseInt(selectObj.options[selectObj.selectedIndex].value);
   
-  return 0;
+  let totalPrice = 0;
+
+  if(eventId ==1){
+    totalPrice = ticknum * 91.25;
+  }else if (eventId ==2){
+    totalPrice = ticknum * 79.88;
+  }else if(eventId ==3){
+    totalPrice = ticknum * 83.12;
+  }else if(eventId ==4){
+    totalPrice = ticknum * 100;
+  }else{
+    totalPrice = 0;
+  }
+  return totalPrice;
 }
 
 
