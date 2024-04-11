1c47736f31de2a10a9e23f0a06839a017a1bdc89
Nontapat <Antauribp@gmail.com>
2024-04-11 13:18:58 +0700

    calucate when book

--- priceCal.js
+++ priceCal.js
@@ -2,8 +2,15 @@ function priceCalculate() {
   let ticknum = (document.getElementById("ticknum").value).trim();
   let selectObj = document.getElementById("event");
   let eventId = parseInt(selectObj.options[selectObj.selectedIndex].value);
+  const eventPrices = {
+    1: 91.25,
+    2: 79.88,
+    3: 83.12,
+    4: 100
+  };
+  let totalPrice = ticknum * eventPrices[eventId];
   
-  return 0;
+  return totalPrice;
 }
 
 
