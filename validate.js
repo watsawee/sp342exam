9dc6e2ea036d0dece729479f6baf755f965695b7
nattaphong kongnang-om <2210511101021@live4.uctt.ac.th>
2024-04-11 10:25:36 +0700

    ticket have 1 - 5 per person. (1.3)

--- validate.js
+++ validate.js
@@ -9,7 +9,7 @@ function checkNID() {
 
 function checkTicketNo() {
   let num = (document.getElementById("ticknum").value).trim();
-  if (isNaN(num)) {
+  if (isNaN(num) || num > 5 || num < 1 ) {
     return false;
   } else {
 	return true;
