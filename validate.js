e908ccff77544e286f3c610bf87c831447f1114e
huhsji <huhaji12@gmail.com>
2024-04-11 11:09:25 +0700

    modified checkID

--- validate.js
+++ validate.js
@@ -1,6 +1,7 @@
 function checkNID() {
   let nid = (document.getElementById("nid").value).trim();
-  if (nid.length != 13) {
+  if (nid.length != 13 || isNaN(nid)) {
+	alert("หมายเลขบัตรประชาชนต้องมี 13 หลักและต้องเป็นตัวเลขเท่านั้น")
     return false;
   } else {
 	return true;
@@ -31,5 +32,5 @@ function validateForm(){
 			alert("Total price for this booking is "+total+" USD");
 			return false;
 		}
-	}
+	}	
 }
