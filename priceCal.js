14b1e8e59889bd6502c7b3543f820661aba0eec4
Nainymerdjungley <azeeace2546@gmail.com>
2024-04-11 12:18:00 +0700

    click booking cal ticket

--- priceCal.js
+++ priceCal.js
@@ -2,8 +2,26 @@ function priceCalculate() {
   let ticknum = (document.getElementById("ticknum").value).trim();
   let selectObj = document.getElementById("event");
   let eventId = parseInt(selectObj.options[selectObj.selectedIndex].value);
-  
-  return 0;
+  let tickerPrice = 0;
+  switch (eventId) {
+    case 1:
+      ticketPrice = 91.25;
+      break;
+    case 2:
+      ticketPrice = 79.88;
+      break;
+    case 3:
+      ticketPrice = 83.12;
+      break;
+    case 4:
+      ticketPrice = 100;
+      break;
+    default:
+      ticketPrice = 0;
+  }
+
+  return ticknum * ticketPrice;
 }
+
 
 
