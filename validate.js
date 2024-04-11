73cd658a05aac4392a3e074c84769fa30ec5042e
kansit <2310511101083@live4.utcc.ac.th>
2024-04-11 10:26:46 +0700

    third commit - fix code ticket can buy no more than 5

--- validate.js
+++ validate.js
@@ -9,7 +9,7 @@ function checkNID() {
 
 function checkTicketNo() {
   let num = (document.getElementById("ticknum").value).trim();
-  if (isNaN(num)) {
+  if (isNaN(num) || num < 1 || num > 5) {
     return false;
   } else {
 	return true;
@@ -23,7 +23,7 @@ function validateForm(){
 	  return false;
 	}else{
 		if(!checkTicketNo()){
-		  alert("Invalid value for No.of tickets!!");
+		  alert("Invalid value for No.of tickets!! Please enter a number between 1 and 5.");
 		  document.getElementById("ticknum").focus();
 		  return false;
 		}else{
