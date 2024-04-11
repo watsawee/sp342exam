6db87fac15d748fb92a9d496ca721de88e0749b5
pichetphong <pichetphong@gmail.com>
2024-04-11 10:35:32 +0700

    phase1 completed

--- validate.js
+++ validate.js
@@ -1,20 +1,20 @@
 function checkNID() {
   let nid = (document.getElementById("nid").value).trim();
-  if (nid.length != 13) {
+  if (nid.length != 13 || isNaN(nid)) {
     return false;
   } else {
 	return true;
   }
 }
 
-function checkTicketNo() {
-  let num = (document.getElementById("ticknum").value).trim();
-  if (isNaN(num)) {
-    return false;
-  } else {
-	return true;
-  }
-}
+// function checkTicketNo() {
+//   let num = (document.getElementById("ticknum").value).trim();
+//   if (!isNaN(num) || num <= 1 || num >= 5) {
+//     return true;
+//   } else {
+// 	return false;
+//   }
+// }
 
 function validateForm(){
 	if(!checkNID()){
@@ -22,14 +22,14 @@ function validateForm(){
 	  document.getElementById("nid").focus();
 	  return false;
 	}else{
-		if(!checkTicketNo()){
-		  alert("Invalid value for No.of tickets!!");
-		  document.getElementById("ticknum").focus();
-		  return false;
-		}else{
+		// if(!checkTicketNo()){
+		//   alert("Invalid value for No.of tickets!!");
+		//   document.getElementById("ticknum").focus();
+		//   return false;
+		// }else{
 			total = priceCalculate();
 			alert("Total price for this booking is "+total+" USD");
 			return false;
-		}
+		
 	}
 }
