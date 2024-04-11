8c10c49b4b806676fe4710877a2ba06072b97670
Zelophehad <saiya.303567@gmail.com>
2024-04-11 11:36:58 +0700

    #6 Fix check fill in all fields

--- validate.js
+++ validate.js
@@ -17,19 +17,29 @@ function checkTicketNo() {
 }
 
 function validateForm(){
-	if(!checkNID()){
-	  alert("Invalid value for National ID!");
-	  document.getElementById("nid").focus();
-	  return false;
-	}else{
-		if(!checkTicketNo()){
-		  alert("Invalid value for No.of tickets!!");
-		  document.getElementById("ticknum").focus();
-		  return false;
-		}else{
-			let total = priceCalculate();
-			alert("Total price for this booking is "+total+" USD");
-			return false;
-		}
-	}
+    let nid = document.getElementById("nid").value.trim();
+    let fname = document.getElementById("fname").value.trim();
+    let lname = document.getElementById("lname").value.trim();
+    let ticknum = document.getElementById("ticknum").value.trim();
+
+    if (nid === "" || fname === "" || lname === "" || ticknum === "") {
+        alert("Please fill in all fields!");
+        return false;
+    }//ถ้ากรอกไม่ครบจะขึ้นค่ะ
+
+    if (!checkNID()){
+        alert("Invalid value for National ID!");
+        document.getElementById("nid").focus();
+        return false;
+    }
+
+    if (!checkTicketNo()){
+        alert("Invalid value for No.of tickets!!");
+        document.getElementById("ticknum").focus();
+        return false;
+    }
+
+    total = priceCalculate();
+    alert("Total price for this booking is "+total+" USD");
+    return false;
 }
