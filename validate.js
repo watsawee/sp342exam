26adcc4dc9eb4f13b9f5f1b7b5f2499081c3871f
Thanat <tk.2546thanat.1@gmail.com>
2024-04-11 10:50:09 +0700

    Fix Validate.js

--- validate.js
+++ validate.js
@@ -1,6 +1,6 @@
 function checkNID() {
   let nid = (document.getElementById("nid").value).trim();
-  if (nid.length != 13) {
+  if (nid.length != 13 || isNaN(nid)) {
     return false;
   } else {
 	return true;
@@ -9,7 +9,7 @@ function checkNID() {
 
 function checkTicketNo() {
   let num = (document.getElementById("ticknum").value).trim();
-  if (isNaN(num)) {
+  if (isNaN(num) || num < 1 || num > 5) {
     return false;
   } else {
 	return true;
